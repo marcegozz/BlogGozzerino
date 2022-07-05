be5576f2ad77481a219618a591e80835a6a2f002
marcegozz <marcellogozzerino@hotmail.com>
2022-07-05 15:03:18 -0300

    Checkpoint

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'vlog',
+    'miembros',
 ]
 
 MIDDLEWARE = [
--- Blog/miembros/apps.py
+++ Blog/miembros/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RegLogConfig(AppConfig):
+class MiembrosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reg_log'
+    name = 'miembros'
