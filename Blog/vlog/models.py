73b2859458ecc157e9f8fe09e6c97fbf537aab6c
marcegozz <marcellogozzerino@hotmail.com>
2022-07-05 03:01:53 -0300

    Checkpoint (Agregar Post)

--- Blog/vlog/models.py
+++ Blog/vlog/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     titulo = models.CharField(max_length=255)
+    etiqueta = models.CharField(max_length=255, default="Blogsito")
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     cuerpo = models.TextField()
 
 #Permite que se lea claramente el titulo y el autor claramente en la pagina de Admin
     def __str__(self):
         return self.titulo + ' | ' + str(self.autor)
+    
+    def get_absolute_url(self):
+        return reverse('ArticuloDetallado', args=(str(self.id)))
 
 
