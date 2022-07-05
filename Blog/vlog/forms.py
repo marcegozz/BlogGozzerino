fc9cfcd2dc69b94a35f440b12f9d7ed8351fcd7e
marcegozz <marcellogozzerino@hotmail.com>
2022-07-05 04:08:15 -0300

    Checkpoint
    Forms emprolijados con Bootstrap

--- Blog/vlog/forms.py
+++ Blog/vlog/forms.py
@@ -1,5 +1,3 @@
-from tkinter.tix import Select
-from turtle import textinput
 from django import forms
 from .models import Post
 
@@ -7,13 +5,13 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('titulo', 'etiqueta', 'autor', 'cuerpo')
-        elementos = {
+        widgets = {
             'titulo' : forms.TextInput(attrs={'class': 'form-control'}),
-            'etiqueta' : forms.TextInput(attrs={'class':'form-control'}),
-            'autor' : forms.Select(attrs={'class':'form-control'}),
-            'cuerpo' : forms.Textarea(attrs={'class':'form-control'}),
+            'etiqueta' : forms.TextInput(attrs={'class': 'form-control'}),
+            'autor' : forms.Select(attrs={'class': 'form-control'}),
+            'cuerpo' : forms.Textarea(attrs={'class': 'form-control'}),
         }
             
 
 
-#https://getbootstrap.com/docs/5.2/forms/form-control/
+#https://getbootstrap.com/docs/5.2/forms/overview/
